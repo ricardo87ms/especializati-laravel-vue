7d00bab5dc9d31cb0dde3edc86697d7b51a9583d
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2020-10-31 08:10:28 -0300

    Configurando vue-router e vuex

--- laravel-webservice/resources/js/app.js
+++ laravel-webservice/resources/js/app.js
@@ -2,8 +2,13 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import router from './routes/routers';
+import store from './vuex/store';
+
 Vue.component('teste-component', require('./components/TesteComponent.vue').default);
 
 const app = new Vue({
+    router,
+    store,
     el: '#app',
 });
